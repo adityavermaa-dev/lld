3bced0862910d84dc3a965ab0478ce6ded9ead87
adit-ya15 <aditya262701@gmail.com>
2026-07-22 23:52:37 +0530

    Modification in abstract design

--- patterns/AbstractFactoryDesign/Main.java
+++ patterns/AbstractFactoryDesign/Main.java
@@ -1,11 +1,12 @@
 package AbstractFactoryDesign;
 
 public class Main {
+
     public static void main(String[] args) {
-        GuiFactory factory = new MacFactory();
-        Button button = factory.createButton();
-        Checkbox checkbox = factory.createCheckbox();
-        button.render();
-        checkbox.render();
+
+        GuiFactory factory = new WindowFactory();
+        Application app = new Application(factory);
+
+        app.renderUI();
     }
 }
